0d2a79eb49440b83e1c6eddb794644ddcab2ddf8
NaganoTomoya <kbc>
2020-02-27 11:24:57 +0900

    エフェクトを追加しました。

--- GameTemplate/Game/ProgressBar.cpp
+++ GameTemplate/Game/ProgressBar.cpp
@@ -13,23 +13,41 @@ ProgressBar::~ProgressBar()
 bool ProgressBar::Start()
 {
 	CVector2 Pivot = { 0.0,0.5 };
-	k_spriteRender = NewGO<prefab::CSpriteRender>(0);
-	k_spriteRender->SetPivot(Pivot);
-	
-	
+	k_spriteRender = NewGO<prefab::CSpriteRender>(0);//唐揚げ
+	k_spriteRender->SetPivot(Pivot);//唐揚げピボット
+
+	p_spriteRender = NewGO<prefab::CSpriteRender>(0);//ポテト
+	p_spriteRender->SetPivot(Pivot);//ポテトピボット
+
+
+
 	return true;
 }
 
 void ProgressBar::Update()
 {
-	k_spriteRender->Init(L"sprite/赤.dds", x, 80.0f);
-	x += 5;
+	k_spriteRender->Init(L"sprite/赤.dds", x, 30.0f);
+	p_spriteRender->Init(L"sprite/悲しみ.dds", y, 30.0f);
+	x += 3.0;
+	y += 2.0;
 	if (x >= 500) {
 		prefab::CEffect* effect = NewGO<prefab::CEffect>(0);
 		CVector3 scale = { 10.0f,1.0f,10.0f };
 		effect->SetScale(scale);
 		effect->Play(L"effect/Bon.efk");
-		effect->SetPosition(k_position);
+		effect->SetPosition(effecto);
 		x = 0.0f;
 	}
+
+	if (y >= 300) {
+		prefab::CEffect* effect = NewGO<prefab::CEffect>(0);
+		CVector3 scale = { 5.0f,1.0f,5.0f };
+		effect->SetScale(scale);
+		effect->Play(L"effect/Poteto.efk");
+		effect->SetPosition(effe);
+		y = 0.0f;
+	}
+	
+	k_spriteRender->SetPosition(k_position);
+	p_spriteRender->SetPosition(p_position);
 }
--- GameTemplate/Game/ProgressBar.h
+++ GameTemplate/Game/ProgressBar.h
@@ -7,8 +7,17 @@ public:
 	bool Start();
 	void Update();
 private:
-	prefab::CSpriteRender* k_spriteRender = nullptr;
-	CVector3 k_position = {0.0,0.0,0.0};
-	float x = 0.0f;
+	prefab::CSpriteRender* k_spriteRender = nullptr;//唐揚げバー
+	prefab::CSpriteRender* p_spriteRender = nullptr;//ポテトバー
+
+	CVector3 k_position = {-580.0,300.0,0.0};//唐揚げバーの位置
+	CVector3 p_position = { -580.0,250.0,0.0 };//ポテトバーの位置
+	
+
+	CVector3 effecto = { 30.0,160.0,0.0 };//唐揚げエフェ位置
+	CVector3 effe = { 70.0,140.0,0.0 };//ポテトエフェ位置
+
+	float x = 0.0f;//赤バー初期値
+	float y = 0.0f;//水玉初期値
 };
 

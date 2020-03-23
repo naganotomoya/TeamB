cfea3436e2790948a9fe56a1e85a878b8d05ce64
NaganoTomoya <kbc>
2020-03-23 14:51:00 +0900

    ポテト追加した。

--- GameTemplate/Game/ScenePoteto.cpp
+++ GameTemplate/Game/ScenePoteto.cpp
@@ -6,3 +6,35 @@ ScenePoteto::ScenePoteto()
 ScenePoteto::~ScenePoteto()
 {
 }
+
+bool ScenePoteto::Start()
+{
+	//ポテト
+	m_Poteto = NewGO<prefab::CSkinModelRender>(0, "Poteto");
+	m_Poteto->Init(L"modelData/poteto.cmo");
+	m_Pposition = { 70.0f,0.0f,0.0f};
+	m_Poteto->SetPosition(m_Pposition);
+	//お皿
+	m_osara = NewGO<prefab::CSkinModelRender>(0, "osara");
+	m_osara->Init(L"modelData/KaraageS/sara.cmo");
+
+	//油
+	//2D表示するときそのままだとでないと思うから3D表示にして反転させてください。
+	m_abura = NewGO<prefab::CSpriteRender>(0);
+	m_abura->Init(
+		L"sprite/Phaikei/Abura.dds",
+		MainCamera().GetWidth(),
+		MainCamera().GetHeight(),
+		true	//3D表示
+	);
+	Hanten.SetRotationDeg(CVector3::AxisY, 180.0f);	//反転
+	m_abura->SetScale({ 0.05f,0.1f,0.05f });
+	m_aburaposition = { -10.0f,0.0f,-20.0f };
+	m_abura->SetPosition(m_aburaposition);
+	m_abura->SetRotation(Hanten);					//反転を適応
+	return true;
+}
+
+void ScenePoteto::Update()
+{
+}
--- GameTemplate/Game/ScenePoteto.h
+++ GameTemplate/Game/ScenePoteto.h
@@ -4,5 +4,14 @@ class ScenePoteto : public IGameObject
 public:
 	ScenePoteto();
 	~ScenePoteto();
+	bool Start();
+	void Update();
+private:
+	prefab::CSkinModelRender* m_Poteto = nullptr;		//スキンモデルレンダラー。
+	prefab::CSkinModelRender* m_osara = nullptr;		//スキルモデルレンダラー。
+	prefab::CSpriteRender* m_abura = nullptr;		//油の２D
+	CQuaternion Hanten;								//回転
+	CVector3 m_aburaposition = CVector3::Zero;		//油のポジション
+	CVector3 m_Pposition = CVector3::Zero;			//生ポテのポジション
 };
 

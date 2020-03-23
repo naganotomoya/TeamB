d4ed04e6d6bcd3d6f2e2959509459ae4382c0705
enzakisousei <kbc19b32@stu.kawahara.ac.jp>
2020-03-23 09:47:36 +0900

    ドリンクバー

--- GameTemplate/Game/SceneDrink.cpp
+++ GameTemplate/Game/SceneDrink.cpp
@@ -7,3 +7,19 @@ SceneDrink::SceneDrink()
 SceneDrink::~SceneDrink()
 {
 }
+bool SceneDrink::Start()
+{
+	//ƒ‚ƒfƒ‹‚ğƒ[ƒh
+	m_skinModelRenderer = NewGO<prefab::CSkinModelRender>(0,"koop1");
+	m_skinModelRenderer->Init(L"modelData/koop1.cmo");
+	m_skinModelRenderer->SetPosition({ -2550.0f,0.0f,0.0f });
+
+	player = FindGO<Player>("player");
+	return true;
+}
+void SceneDrink::Update()
+{
+	
+	CVector3 diff = player->ReturnRPlayerPosition() - m_position;
+
+}
--- GameTemplate/Game/SceneDrink.h
+++ GameTemplate/Game/SceneDrink.h
@@ -1,8 +1,21 @@
 #pragma once
+#include "Player.h"
+
 class SceneDrink : public IGameObject
 {
 public:
 	SceneDrink();
 	~SceneDrink();
+	bool Start();
+	void Update();
+
+	//スプライトレンダラー
+	prefab::CSkinModelRender* m_skinModelRenderer = nullptr;
+
+	//移動
+	CVector3 m_position = CVector3::Zero;
+
+	Player* player = nullptr;
+	//SceneDrink* koop1 = nullptr;
 };
 

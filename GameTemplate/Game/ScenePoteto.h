61e6c3e05cc6abfd3cbe619dcd709e76b82cbb22
NaganoTomoya <kbc>
2020-03-24 17:02:58 +0900

    ポテトとフライヤーの追加した。

--- GameTemplate/Game/ScenePoteto.h
+++ GameTemplate/Game/ScenePoteto.h
@@ -1,17 +1,35 @@
 #pragma once
+class Player;
+class Camera;
 class ScenePoteto : public IGameObject
 {
 public:
 	ScenePoteto();
 	~ScenePoteto();
 	bool Start();
+	void PotetoMove(CVector3& pos);
 	void Update();
 private:
-	prefab::CSkinModelRender* m_Poteto = nullptr;		//スキンモデルレンダラー。
+	Player* m_player = nullptr;
+	Camera* m_camera = nullptr;
+	prefab::CSkinModelRender* m_Poteto = nullptr;		//ポテトスキンモデル。
 	prefab::CSkinModelRender* m_osara = nullptr;		//スキルモデルレンダラー。
+	prefab::CSkinModelRender* m_Flyer = nullptr;		//フライヤースキンモデル。
 	prefab::CSpriteRender* m_abura = nullptr;		//油の２D
+
+
+	
+
 	CQuaternion Hanten;								//回転
 	CVector3 m_aburaposition = CVector3::Zero;		//油のポジション
+
+	CVector3 m_Fposition = CVector3::Zero;			//フライヤーポジション
+	CQuaternion FlyQuater;							//フライやーの回転。
 	CVector3 m_Pposition = CVector3::Zero;			//生ポテのポジション
+	CVector3 m_Oposition = CVector3::Zero;			//お皿のポジション
+	CQuaternion m_Srotation = CQuaternion::Identity;//お皿の回転
+	CVector3 PPdiff = CVector3::Zero;				//ポテトとプレイヤーの手の距離
+
+	bool pushPote = false;		//ポテトを持ったか
 };
 

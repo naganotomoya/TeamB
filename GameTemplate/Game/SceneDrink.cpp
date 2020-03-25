095de7ff31621755fe9d7a5c1fe7b91430666fdc
enzakisousei <kbc19b32@stu.kawahara.ac.jp>
2020-03-25 11:24:27 +0900

    フライヤー2

--- GameTemplate/Game/SceneDrink.cpp
+++ GameTemplate/Game/SceneDrink.cpp
@@ -7,6 +7,7 @@ SceneDrink::SceneDrink()
 SceneDrink::~SceneDrink()
 {
 	DeleteGO(m_kop);
+	DeleteGO(m_kop2);
 	DeleteGO(m_db);
 }
 bool SceneDrink::Start()
@@ -15,6 +16,12 @@ bool SceneDrink::Start()
 	m_kop = NewGO<prefab::CSkinModelRender>(0,"koop1");
 	m_kop->Init(L"modelData/koop1.cmo");
 	m_kop->SetPosition(m_position);
+
+	//モデルをロード
+	m_kop2 = NewGO<prefab::CSkinModelRender>(0, "koop2");
+	m_kop2->Init(L"modelData/koop2.cmo");
+	m_kop2->SetPosition(m_position2);
+	m_kop2->SetScale({ 1.5f,1.5f,1.5f });
 	//モデルをロード
 	m_db = NewGO<prefab::CSkinModelRender>(0, "dorinkuba");
 	m_db->Init(L"modelData/dorinkuba2.cmo");
@@ -28,17 +35,47 @@ bool SceneDrink::Start()
 }
 void SceneDrink::Update()
 {
+	//コップ1の当たり判定
 	m_Pposition = player->ReturnRPlayerPosition();
 	//２点間の距離を計算する。
 	CVector3 diff1 = m_Pposition - m_position;
-	if (diff1.Length() >= 10.0f) {
-		if (Pad(0).IsPress(enButtonB)) {
+	if (diff1.Length() <= 20.0f) {
+		if (Pad(0).IsPress(enButtonB)
+			&& kophave2 == false) {
+			kophave1 = true;
 			/*m_position.x = m_Pposition.x;
 			m_position.y = m_Pposition.y;*/
-
 			player->Move(m_position);
 		}
 	}
 	//CVector3 diff = player->ReturnRPlayerPosition() - m_position;
 	m_kop->SetPosition(m_position);
+
+	//コップ2の当たり判定
+	//m_Pposition2 = player->ReturnRPlayerPosition();
+	//２点間の距離を計算する。
+	CVector3 diff2 = m_Pposition - m_position2;
+	if (diff2.Length() <= 20.0f) {
+		if (Pad(0).IsPress(enButtonB)
+			&& kophave1 == false) {
+			kophave2 = true;
+			/*m_position.x = m_Pposition.x;
+			m_position.y = m_Pposition.y;*/
+
+			player->Move(m_position2);
+		}
+	}
+	//CVector3 diff = player->ReturnRPlayerPosition() - m_position;
+	m_kop2->SetPosition(m_position2);
+
+	if (!Pad(0).IsPress(enButtonB)
+		&& kophave1 == true) {
+		kophave1 = false;
+	}
+	if (!Pad(0).IsPress(enButtonB)
+		&& kophave2 == true) {
+		kophave2 = false;
+	}
+
+
 }
--- GameTemplate/Game/SceneDrink.h
+++ GameTemplate/Game/SceneDrink.h
@@ -13,11 +13,14 @@ public:
 	//prefab::CSkinModelRender* m_skinModelRenderer = nullptr;
 
 	//移動
-	CVector3 m_position = { -2450.0f,-50.0f,5.0f };
+	CVector3 m_position = { -2480.0f,-20.0f,-6.0f };
+	CVector3 m_position2 = { -2480.0f,-50.0f,-6.0f };
 	CVector3 m_Pposition = CVector3::Zero;
-
+	CVector3 m_Pposition2 = CVector3::Zero;
+	
 	//スキンモデルレンダラー
 	prefab::CSkinModelRender* m_kop = nullptr;	
+	prefab::CSkinModelRender* m_kop2 = nullptr;
 	prefab::CSkinModelRender* m_db = nullptr;
 
 	
@@ -25,6 +28,9 @@ public:
 	//CVector3 m_moveSpeed = CVector3::Zero;
 	
 	int m_state = 0;
+
+	bool kophave1 = false;
+	bool kophave2 = false;
 
 	Player* player = nullptr;
 	//SceneDrink* koop1 = nullptr;

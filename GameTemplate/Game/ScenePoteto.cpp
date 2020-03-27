4408eed2c5425e27cb405d52dfdddb000abbdfa8
NaganoTomoya <kbc>
2020-03-27 18:04:31 +0900

    冷凍されたポテトを追加して無限にポテトを揚げれるようになった。

--- GameTemplate/Game/ScenePoteto.cpp
+++ GameTemplate/Game/ScenePoteto.cpp
@@ -10,6 +10,7 @@ ScenePoteto::ScenePoteto()
 ScenePoteto::~ScenePoteto()
 {
 	DeleteGO(m_Poteto);
+	DeleteGO(m_KanseiPoteto);
 	DeleteGO(m_Flyer);
 	DeleteGO(m_osara);
 	DeleteGO(m_abura);
@@ -19,17 +20,23 @@ bool ScenePoteto::Start()
 {
 	m_camera = FindGO<Camera>("camera");
 	m_player = FindGO<Player>("player");
-	//ポテト
+	//生ポテト
 	m_Poteto = NewGO<prefab::CSkinModelRender>(0, "Poteto");
-	m_Poteto->Init(L"modelData/Poteto/poteto.cmo");
+	m_Poteto->Init(L"modelData/icePote/Poteto1.cmo");
 	m_Pposition = { -1230.0f,0.0f,0.0f};
 	m_Poteto->SetPosition(m_Pposition);
+	//ポテト
+	m_KanseiPoteto = NewGO<prefab::CSkinModelRender>(0, "KanseiPoteto");
+	m_KanseiPoteto->Init(L"modelData/Poteto/poteto.cmo");
+	m_KanseiPoteto->SetPosition(m_Kanseiposition);
+	
 	//フライヤー
 	m_Flyer = NewGO<prefab::CSkinModelRender>(1, "Flyer");
 	m_Flyer->Init(L"modelData/Poteto/huraiya-4.cmo");
 	FlyQuater.SetRotationDeg(CVector3::AxisX, 90.0f);
 	m_Flyer->SetRotation(FlyQuater);
-	m_Fposition = { -1280.0f,0.0f,-20.0f };
+	m_Flyer->SetScale({ 1.5f,1.25f,1.25f });
+	m_Fposition = { -1280.0f,-20.0f,-30.0f };
 	m_Flyer->SetPosition(m_Fposition);
 	//お皿
 	m_osara = NewGO<prefab::CSkinModelRender>(0, "osara");
@@ -49,8 +56,8 @@ bool ScenePoteto::Start()
 		true	//3D表示
 	);
 	Hanten.SetRotationDeg(CVector3::AxisY, 180.0f);	//反転
-	m_abura->SetScale({ 0.02f,0.0375f,0.05f });
-	m_aburaposition = { -1281.5f,14.6f,1.5f };
+	m_abura->SetScale({ 0.031f,0.05f,0.05f });
+	m_aburaposition = { -1282.0f,4.0f,-5.0f };
 	m_abura->SetPosition(m_aburaposition);
 	m_abura->SetRotation(Hanten);					//反転を適応
 
@@ -62,6 +69,7 @@ bool ScenePoteto::Start()
 
 void ScenePoteto::Update()
 {
+	PoteFly += GameTime().GetFrameDeltaTime();//お時間です。
 	//なにもない状態
 	if (m_state == State_None) {
 		if (Pad(0).IsPress(enButtonB)) {
@@ -71,8 +79,6 @@ void ScenePoteto::Update()
 
 	//ポテトを持っている状態
 	else if (m_state == State_PickPoteto) {
-		//②手とポテトの距離が近くなったら、
-
 		//プレイヤーの手とポテトの距離を測定
 		PPdiff = m_player->ReturnRPlayerPosition() - m_Pposition;
 		//プレイヤーの手の当たり判定計算
@@ -80,27 +86,54 @@ void ScenePoteto::Update()
 		//距離３０以下でBボタンが押されたら
 		if (len <= 30.0f && Pad(0).IsPress(enButtonB)) {
 			m_player->Move(m_Pposition);
-			m_Poteto->SetPosition(m_Pposition);
+			if (Nama == true) {
+				m_Poteto->SetPosition(m_Pposition);
+			}
+			else {
+				m_KanseiPoteto->SetPosition(m_Pposition);
+			}
+			
 		}
 
 		//ポテトとお皿の距離が近くなったら
 		PTdiff = m_Pposition - m_Oposition;
 		//当たり判定
 		len = PTdiff.Length();
+
+		//ポテトと油の距離が近くなったら
+		APdiff = m_Pposition - m_aburaposition;
+		//当たり判定
+		float Len = APdiff.Length();
+
 		//IsActive()はDeleteGOされたらFalse　NewGOされたらTrueなるフラグ
-		if (len <= 20.0f && !Pad(0).IsPress(enButtonB) && m_Poteto->IsActive() == true) {
+		if (len <= 20.0f && !Pad(0).IsPress(enButtonB) && m_KanseiPoteto->IsActive() == true) {
 			bool Ispoteto = true;
 			//お皿とポテトの距離が20.0以下でBボタンが押されてなくて、ポテトのアクティブがTrueなら
 			m_state = State_TranlateFlyer;
 		}
-		//ポテトと油の距離が近くなったら
-		APdiff = m_Pposition - m_aburaposition;
-		float Len = APdiff.Length();
-		if (Len <= 20.0f && !Pad(0).IsPress(enButtonB)) {
-			PoteFly += GameTime().GetFrameDeltaTime();
-			if (PoteFly >= 4) {
-				//揚げたてポテトをNewGOする
-			}
+
+		//ポテトと油の距離が近くなったら揚げている状態にループ
+		else if (Len <= 20.0f && !Pad(0).IsPress(enButtonB) && m_Poteto->IsActive() == true) {
+			m_state = State_oilinput;
+		}
+	
+	}
+
+	//ポテトを揚げている状態
+	else if (m_state == State_oilinput) {
+
+		if (PoteFly >= 4 ) {
+			//生ポテとを削除する。
+			DeleteGO(m_Poteto);
+			Nama = false;				//生のポテトがなくなった.
+			//揚げたてポテトを生成
+			m_KanseiPoteto = NewGO<prefab::CSkinModelRender>(0, "KanseiPoteto");
+			m_KanseiPoteto->Init(L"modelData/Poteto/poteto.cmo");
+			Fried = true;				//ポテトがあるよ。
+			//揚げたてポテトをNewGOする
+			m_KanseiPoteto->SetPosition(m_Pposition);
+			//揚げたポテトを持ちたい
+			m_state = State_PickPoteto;
 		}
 	}
 
@@ -108,24 +141,15 @@ void ScenePoteto::Update()
 	else if (m_state == State_TranlateFlyer) {
 		//ステートが盛り付ける状態なら
 		//ポテトのオブジェクトを削除
-		DeleteGO(m_Poteto);
+		DeleteGO(m_KanseiPoteto);
+		//生ポテトを生成
+		m_Poteto = NewGO<prefab::CSkinModelRender>(0, "Poteto");
+		m_Poteto->Init(L"modelData/icePote/Poteto1.cmo");
+		m_Pposition = InitialPosition;
+		m_Poteto->SetPosition(m_Pposition);
+		Nama = true;				//生のポテトがある。
 		//ステートを何もしてない状態に変更
 		m_state = State_None;
 	}
-
-	//else if (m_state == State_TranlateFlyer) {
-		//ポテトとお皿の距離が近くなったら
-		//PTdiff = m_Pposition - m_Oposition;
-		//float len = PTdiff.Length();
-		//if (PTdiff.Length() <= 20.0f && !Pad(0).IsPress(enButtonB)) {
-		//	int a = 0;
-		//	if (m_Poteto != nullptr) {
-		//		DeleteGO(m_Poteto);
-		//		m_Poteto = nullptr;
-		//	}
-
-		//}
-	//}
-
 
 }

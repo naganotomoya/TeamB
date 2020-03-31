ae6121bf579960634f9392aba7a947830497ce83
NaganoTomoya <kbc>
2020-03-31 14:53:22 +0900

    ちょっとだけ変えた。

--- GameTemplate/Game/SceneAraimono.cpp
+++ GameTemplate/Game/SceneAraimono.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "SceneAraimono.h"
 #include "Player.h"
+#include "Camera.h"
 SceneAraimono::SceneAraimono()
 {
 }
@@ -11,23 +12,51 @@ SceneAraimono::~SceneAraimono()
 
 bool SceneAraimono::Start()
 {
+	m_camera = FindGO<Camera>("camera");
 	m_player = FindGO<Player>("player");
 	//お皿
 	m_osara = NewGO<prefab::CSkinModelRender>(0, "osara");
 	m_osara->Init(L"modelData/KaraageS/sara.cmo");//お皿のロード
 	m_Srotation.SetRotationDeg(CVector3::AxisX, 90.0f);//90度回転
 	m_osara->SetRotation(m_Srotation);
-	m_Oposition = { -5110.0f,0.0f,0.0f };			  //初期位置
+	m_Oposition = { -5110.0f,0.0f,-1.0f };			  //初期位置
 	m_osara->SetPosition(m_Oposition);
+
 	return true;
+}
+
+void SceneAraimono::Osara()
+{
+	m_Oposition = m_player->ReturnLPlayerPosition();
+	m_Oposition.x += 5.0f;
+	m_Oposition.y += 9.0f;
+	m_Oposition.z -= 0.5f;
+	m_osara->SetPosition(m_Oposition);
 }
 
 void SceneAraimono::Update()
 {
-	//左手で持って右手で洗う。
-	//左手固定の右手のアニメーションであらう？
-	if (Pad(0).IsPress(enButtonB) && Pad(0).GetLStickYF()) {
-		//お皿を洗う。
-
+	Nowscene = m_camera->ReturnNowScene();
+	if (Nowscene == 4) {
+		Osara();
 	}
+	
+	//左手で持って右手で洗う。
+	m_player->LosaraAnime();
+	m_player->SetLPosition({ -5110.0f,0.0f,-3.0f });
+	//右手とお皿の距離を測る。
+	ROdiff = m_player->ReturnRPlayerPosition() - m_Oposition;
+	//左手固定の右手のアニメーションであらう？
+	//右手のお皿の距離が30以下＆PadのBボタンが押されたらとスティック
+	if ( ROdiff.Length()<=30.0 && Pad(0).IsPress(enButtonB)) {
+		Araimo += GameTime().GetFrameDeltaTime();
+		
+	}
+	if (Araimo >= 5.0) {
+		m_osara->SetPosition(KPos);
+		KanseiSara++;				//完成皿を増やす。
+		Araimo = 0.0f;
+	}
+	/*ある一定のカウントを超えたらお皿一枚が洗えた。
+	次のお皿を初期位置に用意する。&& Pad(0).GetLStickYF()*/
 }
--- GameTemplate/Game/SceneAraimono.h
+++ GameTemplate/Game/SceneAraimono.h
@@ -1,17 +1,26 @@
 #pragma once
 class Player;
+class Camera;
 class SceneAraimono : public IGameObject
 {
 public:
 	SceneAraimono();
 	~SceneAraimono();
 	bool Start();
+	void Osara();
 	void Update();
 private:
 	Player* m_player = nullptr;
+	Camera* m_camera = nullptr;
 	prefab::CSkinModelRender* m_osara = nullptr;//お皿のスキンモデル
 	CVector3 m_Oposition = CVector3::Zero;			//お皿のポジション
 	CQuaternion m_Srotation = CQuaternion::Identity;//お皿の回転
 
+	CVector3 KPos = CVector3{ -5080.0f,0.0f,0.0f };	//洗えた皿のポジション	
+	CVector3 ROdiff = CVector3::Zero;				//お皿と右手の距離
+	float Araimo = 0.0;								//時間
+
+	float KanseiSara = 0.0;							//洗えたお皿
+	int Nowscene = 0;
 };
 

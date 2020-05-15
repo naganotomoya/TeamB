562f8ca2f624f95f89abb83aa9adb15d4a7316f4
enzakisousei <kbc19b32@stu.kawahara.ac.jp>
2020-05-15 13:39:37 +0900

    背景

--- GameTemplate/Game/SceneKaikei.cpp
+++ GameTemplate/Game/SceneKaikei.cpp
@@ -5,4 +5,19 @@ SceneKaikei::SceneKaikei()
 }
 SceneKaikei::~SceneKaikei()
 {
+
+}
+
+bool SceneKaikei::Start()
+{
+	//ƒ‚ƒfƒ‹‚ğƒ[ƒh
+	m_hito = NewGO<prefab::CSkinModelRender>(0, "hito1");
+	m_hito->Init(L"modelData/hito1.cmo");
+	m_hito->SetPosition(m_position);
+
+	return true;
+}
+void SceneKaikei::Update()
+{
+
 }
